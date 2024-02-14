166d70cc4d93a3fd69793ce554f845abf847a78c
morla003 <77115526+muuskrat@users.noreply.github.com>
2024-02-13 21:45:07 -0800

    Update script.js
    Added walls

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 const  BALLZ = [];
+const WALLZ = [];
 
 let RIGHT, LEFT, UP, DOWN;
 let friction = 0.01;
+
 
 class Vector{
     constructor(x, y){
@@ -54,13 +56,21 @@ class Vector{
 
 
 class Ball{
-    constructor(x, y, r){
+    constructor(x, y, r, m){
 
         this.pos = new Vector(x,y);
         this.r = r;
+        this.m = m;
+        if(this.m === 0){
+            this.inv_m = 0;
+        }
+        else{
+            this.inv_m = 1 / this.m;
+        }
+        this.elasticity = 1;
         this.vel = new Vector(0,0);
         this.acc = new Vector(0,0);
-        this.acceleration = .1;
+        this.acceleration = .075;
         this.player = false;
         BALLZ.push(this);
     }
@@ -79,6 +89,10 @@ class Ball{
 
         this.vel.drawVec(this.pos.x, this.pos.y, 25, "blue")
         this.acc.drawVec(this.pos.x, this.pos.y, 250, "green")
+
+        ctx.fillStyle = "black";
+        ctx.fillText("m = "+this.m, this.pos.x-10, this.pos.y-5);
+        ctx.fillText("e = "+this.elasticity, this.pos.x-10, this.pos.y+5);
         
     }
 
@@ -87,6 +101,41 @@ class Ball{
         this.vel = this.vel.add(this.acc);
         this.vel = this.vel.mult(1-friction);
         this.pos = this.pos.add(this.vel);
+    }
+}
+
+class Wall{
+    constructor(x1, y1, x2, y2){
+        this.start = new Vector(x1, y1);
+        this.end = new Vector(x2, y2);
+        WALLZ.push(this);
+    }
+
+    drawWall(){
+        ctx.beginPath();
+        ctx.moveTo(this.start.x, this.start.y);
+        ctx.lineTo(this.end.x, this.end.y);
+        ctx.strokeStyle = "black";
+        ctx.stroke();
+    }
+
+    wallUnit(){
+        return this.end.subtr(this.start).unit();
+    }
+}
+
+class Matrix{
+    constructor(rows, cols){
+        this.rows = rows;
+        this.cols = cols;
+        this.data = [];
+
+        for(let i = 0; i < this.rows; i++){
+            this.data[i] = [];
+            for(let j = 0; j < this.cols; j++){
+                this.data[i][j] = 0;
+            }
+        }
     }
 }
 
@@ -155,6 +204,30 @@ function round(num, prec){
     return Math.round(num * factor)/factor;
 }
 
+function randInt(min, max){
+    return Math.floor(Math.random() * (max-min+1)) + min;
+}
+
+function closestPointBW(b1, w1){
+    let ballToWallStart = w1.start.subtr(b1.pos);
+    if(Vector.dot(w1.wallUnit(), ballToWallStart) > 0){
+        return w1.start;
+    }
+
+    let wallEndToBall = b1.pos.subtr(w1.end);
+    if(Vector.dot(w1.wallUnit(), wallEndToBall) > 0){
+        return w1.end;
+    }
+
+    let closestDist = Vector.dot(w1.wallUnit(), ballToWallStart);
+    let closestVect = w1.wallUnit().mult(closestDist);
+    return w1.start.subtr(closestVect);
+
+
+
+    
+}
+
 function coll_det_bb(b1, b2){
     if(b1.r + b2.r >= b2.pos.subtr(b1.pos).mag()){
         return true;
@@ -164,23 +237,51 @@ function coll_det_bb(b1, b2){
     }
 }
 
+function coll_det_bw(b1, w1){
+    let ballToClosest = closestPointBW(b1, w1).subtr(b1.pos);
+    if(ballToClosest.mag() <= b1.r){
+        return true;
+    }
+}
+
 function pen_res_bb(b1, b2){
     let dist = b1.pos.subtr(b2.pos);
     let pen_depth = b1.r + b2.r - dist.mag();
-    let pen_res = dist.unit().mult(pen_depth/2);
-    b1.pos = b1.pos.add(pen_res);
-    b2.pos = b2.pos.add(pen_res.mult(-1));
+    let pen_res = dist.unit().mult(pen_depth / (b1.inv_m + b2.inv_m));
+
+    b1.pos = b1.pos.add(pen_res.mult(b1.inv_m));
+    b2.pos = b2.pos.add(pen_res.mult(-b2.inv_m));
+}
+
+function pen_res_bw(b1, w1){
+    let penVect = b1.pos.subtr(closestPointBW(b1, w1));
+    b1.pos = b1.pos.add(penVect.unit().mult(b1.r-penVect.mag()));
 }
 
 function coll_res_bb(b1, b2){
     let normal = b1.pos.subtr(b2.pos).unit();
     let relVel = b1.vel.subtr(b2.vel);
     let sepVel = Vector.dot(relVel, normal);
-    let new_sepVel = -sepVel;
+    let new_sepVel = -sepVel * Math.min(b1.elasticity, b2.elasticity);
     let sepVelVec = normal.mult(new_sepVel);
 
-    b1.vel = b1.vel.add(sepVelVec);
-    b2.vel = b2.vel.add(sepVelVec.mult(-1));
+    let vsep_diff = new_sepVel - sepVel;
+    let impulse = vsep_diff / (b1.inv_m + b2.inv_m);
+    let impulseVec = normal.mult(impulse);
+
+
+
+    b1.vel = b1.vel.add(impulseVec.mult(b1.inv_m));
+    b2.vel = b2.vel.add(impulseVec.mult(-b2.inv_m));
+}
+
+function coll_res_bw(b1, w1){
+    let normal = b1.pos.subtr(closestPointBW(b1, w1)).unit();
+    let sepVel = Vector.dot(b1.vel, normal);
+    let new_sepVel = -sepVel * b1.elasticity;
+    let vsep_diff = sepVel - new_sepVel;
+    b1.vel = b1.vel.add(normal.mult(-vsep_diff));
+
 }
 
 
@@ -197,22 +298,54 @@ function mainLoop(timestamp) {
                 coll_res_bb(BALLZ[index], BALLZ[i]);
             }
         }
+        WALLZ.forEach((w) => {
+            if(coll_det_bw(BALLZ[index], w)){
+                pen_res_bw(BALLZ[index], w);
+                coll_res_bw(BALLZ[index], w)
+            }
+        })
+
         b.reposition();
         b.display();
         
     });
 
+
+    WALLZ.forEach((w) => {
+        w.drawWall();
+    });
+
+
+
+    
+
     requestAnimationFrame(mainLoop);
-  }
+    }
  
 
 
-  let Ball1 = new Ball(200, 200, 30);
-  let Ball2 = new Ball(300, 250, 40);
-  let Ball3 = new Ball(400, 250, 35);
-  Ball1.player = true;
+let Ball1 = new Ball(300, 100, 30, 10);
+let Ball2 = new Ball(200, 150, 30, 10);
+let edge1 = new Wall(0, 0, canvas.clientWidth, 0); 
+let edge2 = new Wall(canvas.clientWidth, 0, canvas.clientWidth, canvas.clientHeight);
+let edge3 = new Wall(canvas.clientWidth, canvas.clientHeight, 0, canvas.clientHeight); 
+let edge4 = new Wall(0, canvas.clientHeight, 0, 0);
+/*
+let Wall1 = new Wall(200, 200, 400, 300);
+let Wall2 = new Wall(350, 450, 450, 300);
+*/
+/*
+for(let i = 0; i < 10; i++){
+    let newBall = new Ball(randInt(100, 500), randInt(50,400), 
+        randInt(20,50), randInt(0,10));
 
-  requestAnimationFrame(mainLoop);
+    newBall.elasticity = randInt(0,10) / 10;
+}
+*/
+Ball1.player = true;
+
+
+requestAnimationFrame(mainLoop);
 
 
 
@@ -233,3 +366,8 @@ setInterval(function(){
     drawBall(x, y, 20);
 }, 1000/60);
 */
+
+
+/*closestPointBW(Ball1, Wall1).subtr(Ball1.pos)
+        .drawVec(Ball1.pos.x, Ball1.pos.y, 1, "red");
+    */
